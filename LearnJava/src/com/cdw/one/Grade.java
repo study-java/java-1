302ed4e9261192e1e0471b199c4b1c19752ffd13
xiaomagecdw <918971505@qq.com>
2019-05-07 22:02:58 +0800

    第一天脚本

---
+++ LearnJava/src/com/cdw/one/Grade.java
@@ -0,0 +1,19 @@
+package com.cdw.one;
+
+import java.util.Scanner;
+
+public class Grade {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请出入小明上一次成绩：");
+		int gr1 = scanner.nextInt();
+		System.out.print("请出入小明这一次成绩：");
+		int gr2 = scanner.nextInt();
+		double f1 = gr2%gr1;
+		System.out.print("成绩提高的百分比为:");
+		System.out.printf("%.2f%%\n",f1);
+	}
+
+}
